e7c941dc8f207ac3b566631165b9cbfd385fc9f0
Abraham Gift <abrahamgift788@gmail.com>
2026-07-09 16:33:02 +0100

    Refactor Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -2,11 +2,11 @@
 // SUPABASE CONFIG
 // ===============================
 
-const SUPABASE_URL = "https://jikupeerqizcthwargvi.supabase.co";
+const SUPABASE_URL =
+"https://jikupeerqizcthwargvi.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_MgvlEUd8LDIPk-ECIOpQrQ_RJZzXamO";
+const SUPABASE_KEY =
+"sb_publishable_MgvlEUd8LDIPk-ECIOpQrQ_RJZzXamO";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const supabaseClient =
+supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
