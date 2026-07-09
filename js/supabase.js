110973a9a399c1a6eda120885d7fbd2ae432bc74
Abraham Gift <abrahamgift788@gmail.com>
2026-07-09 16:11:06 +0100

    Add Supabase configuration in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+// ===============================
+// SUPABASE CONFIG
+// ===============================
+
+const SUPABASE_URL = "https://jikupeerqizcthwargvi.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_MgvlEUd8LDIPk-ECIOpQrQ_RJZzXamO";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
